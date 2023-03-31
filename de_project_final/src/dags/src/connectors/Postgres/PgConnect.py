c0524ff8b2d2b375485fa00240b256cd29964c52
Rashit Gafarov <rashit.r.gafarov@gmail.com>
2023-03-31 13:51:59 +0300

    minor fixes

--- de_project_final/src/dags/src/connectors/Postgres/PgConnect.py
+++ de_project_final/src/dags/src/connectors/Postgres/PgConnect.py
@@ -3,10 +3,6 @@ from typing import Generator
 from sqlalchemy import create_engine
 from sqlalchemy.exc import OperationalError
 
-# engine = create_engine("postgresql+psycopg2://student:de_student_112022@rc1b-w5d285tmxa8jimyn.mdb.yandexcloud.net:6432/db1")
-# dbConnection = engine.connect()
-
-
 class PgConnect:
     def __init__(self, host: str, port: int, db_name: str, user: str, password: str) -> None:
         self.host = host
@@ -21,17 +17,3 @@ class PgConnect:
     def connection(self):
         self._engine = create_engine(self.url())
         return self._engine.connect()
-
-
-    # @contextmanager
-    # def connection(self):
-    #     engine = create_engine(self.url())
-    #     conn = engine.connect()
-    #     try:
-    #         yield conn
-    #         conn.commit()
-    #     except Exception as e:
-    #         conn.rollback()
-    #         raise e
-    #     finally:
-    #         conn.close()
